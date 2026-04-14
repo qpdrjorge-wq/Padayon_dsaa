cd6a3d87bcfabf044bdedf983613cbfa92abcfdf
Sir Tapos <qpdrjorge@tip.edu.ph>
2026-04-15 00:39:58 +0800

    Update AssetSetter.java

--- AssetSetter.java
+++ AssetSetter.java
@@ -1,10 +1,7 @@
 package main;
 
 import entity.*;
-import object.House;
-import object.LumangBahay;
 import object.OBJ_Key;
-import object.Store;
 
 public class AssetSetter {
 
@@ -35,6 +32,10 @@ public class AssetSetter {
         gp.npc[2] = new NPC_Cat(gp);
         gp.npc[2].worldX = 39 * gp.tileSize;
         gp.npc[2].worldY = 22 * gp.tileSize;
+
+        gp.npc[3] = new NPC_Dealer(gp);
+        gp.npc[3].worldX = 28 * gp.tileSize;
+        gp.npc[3].worldY = 33 * gp.tileSize;
     }
 
     public void setNPCChapter2(){
@@ -44,7 +45,7 @@ public class AssetSetter {
         }
 
         gp.npc[0] = new NPC_Dealer(gp);
-        gp.npc[0].worldX = 9 * gp.tileSize;
+        gp.npc[0].worldX = 6 * gp.tileSize;
         gp.npc[0].worldY = 13 * gp.tileSize;
     }
 
@@ -55,12 +56,49 @@ public class AssetSetter {
         }
 
         gp.npc[0] = new NPC_Chismosa(gp);
-        gp.npc[0].worldX = 18 * gp.tileSize;
-        gp.npc[0].worldY = 35 * gp.tileSize;
+        gp.npc[0].worldX = 21 * gp.tileSize;
+        gp.npc[0].worldY = 38 * gp.tileSize;
 
+        gp.npc[1] = new NPC_Jumpscare(gp);
+        gp.npc[1].worldX = 24 * gp.tileSize;   // ← pick your tile coordinates
+        gp.npc[1].worldY = 31 * gp.tileSize;
+
+        gp.npc[2] = new NPC_Jo(gp);
+        gp.npc[2].worldX = 15 * gp.tileSize;   // ← pick your tile coordinates
+        gp.npc[2].worldY = 42 * gp.tileSize;
+
+        gp.npc[3] = new NPC_Girlie(gp);
+        gp.npc[3].worldX = 51 * gp.tileSize;
+        gp.npc[3].worldY = 29 * gp.tileSize;
     }
 
     public void setWheelForChapter3(){
+        int col = 72;
+        int row = 5;
+
+        gp.wheel.x = col * gp.tileSize;
+        gp.wheel.y = row * gp.tileSize;
+
+        gp.obj[0].worldX = col * gp.tileSize;
+        gp.obj[0].worldY = row * gp.tileSize;
+    }
+
+    public void setNPCChapter4(){
+
+        for(int i = 0; i < gp.npc.length; i++){
+            gp.npc[i] = null;
+        }
+
+        gp.npc[0] = new NPC_Eyes(gp);
+        gp.npc[0].worldX = 59 * gp.tileSize;
+        gp.npc[0].worldY = 8 * gp.tileSize;
+
+        gp.npc[1] = new NPC_Eyes2(gp);
+        gp.npc[1].worldX = 35 * gp.tileSize;
+        gp.npc[1].worldY = 8 * gp.tileSize;
+    }
+
+    public void setWheelForChapter4(){
         int col = 85;
         int row = 36;
 
@@ -70,28 +108,5 @@ public class AssetSetter {
         gp.obj[0].worldX = col * gp.tileSize;
         gp.obj[0].worldY = row * gp.tileSize;
     }
-
-
-//    public void setNPCChapter4(){
-//
-//        for(int i = 0; i < gp.npc.length; i++){
-//            gp.npc[i] = null;
-//        }
-//
-//        gp.npc[0] = new NPC_Eyes(gp);
-//        gp.npc[0].worldX = 56 * gp.tileSize;
-//        gp.npc[0].worldY = 8 * gp.tileSize;
-//    }
-//
-//    public void setWheelForChapter4(){
-//        int col = 85;
-//        int row = 36;
-//
-//        gp.wheel.x = col * gp.tileSize;
-//        gp.wheel.y = row * gp.tileSize;
-//
-//        gp.obj[0].worldX = col * gp.tileSize;
-//        gp.obj[0].worldY = row * gp.tileSize;
-//    }
 
 }
